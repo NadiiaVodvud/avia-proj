2c74e38513aa51cc40868d94fa94f07e9bad8789
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-12-30 02:08:51 +0200

    add back to top

---
+++ src/js/to-top.js
@@ -0,0 +1,33 @@
+// const heroContainer = document.querySelector('.container__hero');
+// const backToTopBtn = document.querySelector('.back-to-top');
+// window.addEventListener('scroll', onscroll);
+
+// heroContainer.insertAdjacentElement('beforeend', renderMarcupBtn());
+
+// function renderMarcupBtn() {
+//   return `<button class="back-to-top"><img
+//       class="back-to-top__img"
+//       src="./images/to-top.png"
+//       alt="Air plane"
+//       width="30"
+//       height="30"
+//     /></button>`.join('');
+// }
+
+// function onscroll() {
+//   if (window.pageYOffset > 300) {
+//     backToTopBtn.style.display = 'block';
+//   }
+//   backToTopBtn.style.display = 'none';
+//   //   return;
+// }
+
+// backToTopBtn.addEventListener('click', backToTop);
+
+// function backToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+//   // window.scrollTo(0, 0);
+// }
